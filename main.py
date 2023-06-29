59c35e8b00a969df5483e81f5ea2987a73c66a8c
HM22-code <H.M-22@outlook.fr>
2023-06-29 22:23:25 +0200

    Fix Docstring

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ class main:
     def __init__(self):
         self.filename = None
 
+    def select_file(self):
         """ Open file dialog and show selected file
         """
-    def select_file(self):
         filetypes = (
             ('image files', '.png'),
             ('image files', '.jpg'),
@@ -28,14 +28,14 @@ class main:
             message=self.filename
         )
 
+        
+    def multiplicate(self):
         """ Multiplicate an image inside herself
         """
-    def multiplicate(self):
         photo=Image.open(self.filename)
-        '''On récupere les dimensions de l'image'''
+        # Get image size
         L,H=photo.size
         iterations=int(input("Entrez le nombre d'itérations"))
-        
         for n in range(0,iterations,1):
             for y in range(0,H,1):
                 for x in range(0,L,1):
@@ -48,27 +48,26 @@ class main:
                     x3=x2+L//2
                     photo.putpixel((x3,y2),(r,v,b))
                     # Copy of the top side of image reduced to the bottom side
-            
             for j in range(0,H//2,1):
                 for i in range(0,L,1):
                     ro,ve,bl=photo.getpixel((i,j))
                     photo.putpixel((i,j+H//2),(ro,ve,bl))
-
         photo.show()
 
     def invert(self):
+        """ Invert Colors of image
+        """
         image=Image.open(self.filename)
         image2 = PIL.ImageOps.invert(image)
         image2.show()
 
+    def transparancy(self):
         """ Remove white pixel and replace it with transparent pixel
         """
-    def transparancy(self):
         image=Image.open(self.filename)
         L,H=image.size
         image1=image.convert("RGBA")
         image2=Image.new("RGBA",(L,H))
-        
         for x in range (L):
             for y in range (H):
                 p=image1.getpixel((x,y))
@@ -77,53 +76,47 @@ class main:
                 else :
                     a=255
                 image2.putpixel((x,y),(p[0],p[1],p[2],a))
-        
         #image2.save("filename.png")
         image2.show()
 
     def run(self):
+        """ Main function
+        """
         # create the root window
         root = tk.Tk()
         root.title('Tkinter Open File Dialog')
         root.resizable(False, False)
         root.geometry('300x150')
         root.eval('tk::PlaceWindow . center')
-
         # open button
         open_button = ttk.Button(
             root,
             text='Open a File',
             command=self.select_file
         )
-
         # multiplicate button
         multiplicate_button = ttk.Button(
             root,
             text='Multiplicate',
             command=self.multiplicate
         )
-
         # invert button
         invert_button = ttk.Button(
             root,
             text='Invert',
             command=self.invert
         )
-
         # transparency button
         transparency_button = ttk.Button(
             root,
             text='Transparancy',
             command=self.transparancy
         )
-
         # pack widgets
         open_button.pack(expand=True)
         multiplicate_button.pack(expand=True)
         invert_button.pack(expand=True)
         transparency_button.pack(expand=True)
-
-
         # run the application
         root.mainloop()
 
