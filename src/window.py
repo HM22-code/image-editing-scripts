89c73b4748280b87d438381eba1c957edbd8c9ab
HM22-code <H.M-22@outlook.fr>
2024-09-13 12:23:52 +0200

    Fix gif handling

--- src/window.py
+++ src/window.py
@@ -9,8 +9,8 @@ from PIL import Image, ImageTk
 class Window:
     def __init__(self):
         self.filename = None
-        self.original_image = None
-        self.filtered_image = None
+        self.image = None
+        self.preview = None
         self.root = tk.Tk()
         self.root.title(config.TITLE)
         self.root.iconbitmap(config.ICONBITMAP)
@@ -24,36 +24,37 @@ class Window:
         """
         self.canvas = tk.Canvas(self.root, width=500, height=500)
         self.canvas.pack()
+
         open_button = ttk.Button(self.root, text='Open a File', command=self.select_files)
         open_button.pack(expand=True)
-        multiplicate_button = ttk.Button(self.root, text='Multiplicate', command= lambda: edit.multiplicate(self.filtered_image))
+        multiplicate_button = ttk.Button(self.root, text='Multiplicate', command= lambda: edit.multiplicate(self.image))
         multiplicate_button.pack(expand=True)
-        invert_button = ttk.Button(self.root, text='Invert', command= lambda: edit.invert(self.filtered_image))
+        invert_button = ttk.Button(self.root, text='Invert', command= lambda: edit.invert(self.image))
         invert_button.pack(expand=True)
-        transparency_button = ttk.Button(self.root, text='Transparency', command= lambda: edit.transparency(self.filtered_image))
+        transparency_button = ttk.Button(self.root, text='Transparency', command= lambda: edit.transparency(self.image))
         transparency_button.pack(expand=True)
-        edge_ehance_button = ttk.Button(self.root, text='Edge Ehance', command= lambda: edit.edge_ehance(self.filtered_image))
+        edge_ehance_button = ttk.Button(self.root, text='Edge Ehance', command= lambda: edit.edge_ehance(self.image))
         edge_ehance_button.pack(expand=True)
-        emboss_button = ttk.Button(self.root, text='Emboss', command= lambda: edit.emboss(self.filtered_image))
+        emboss_button = ttk.Button(self.root, text='Emboss', command= lambda: edit.emboss(self.image))
         emboss_button.pack(expand=True)
-        emboss_gif_button = ttk.Button(self.root, text='Emboss_GIF', command= lambda: edit.emboss_gif(self.filtered_image))
+        emboss_gif_button = ttk.Button(self.root, text='Emboss_GIF', command= lambda: edit.emboss_gif(self.image))
         emboss_gif_button.pack(expand=True)
-        pixelate_button = ttk.Button(self.root, text='Pixelate', command= lambda: edit.pixelate(self.filtered_image))
+        pixelate_button = ttk.Button(self.root, text='Pixelate', command= lambda: edit.pixelate(self.image))
         pixelate_button.pack(expand=True)
-        pixelate_gif_button = ttk.Button(self.root, text='Pixelate_GIF', command= lambda: edit.pixelate_gif(self.filtered_image))
+        pixelate_gif_button = ttk.Button(self.root, text='Pixelate_GIF', command= lambda: edit.pixelate_gif(self.image))
         pixelate_gif_button.pack(expand=True)
-        extract_img_gif_button = ttk.Button(self.root, text='Extract_GIF', command= lambda: edit.extract_img_gif(self.filtered_image))
+        extract_img_gif_button = ttk.Button(self.root, text='Extract_GIF', command= lambda: edit.extract_img_gif(self.image))
         extract_img_gif_button.pack(expand=True)
-        comic_button = ttk.Button(self.root, text='Comic', command= lambda: edit.comic(self.filtered_image))
+        comic_button = ttk.Button(self.root, text='Comic', command= lambda: edit.comic(self.image))
         comic_button.pack(expand=True)
-        comic_gif_button = ttk.Button(self.root, text='Comic_GIF', command= lambda: edit.comic_gif(self.filtered_image))
+        comic_gif_button = ttk.Button(self.root, text='Comic_GIF', command= lambda: edit.comic_gif(self.image))
         comic_gif_button.pack(expand=True)
-        segmented_button = ttk.Button(self.root, text='Segmented', command= lambda: edit.segmented(self.filtered_image))
+        segmented_button = ttk.Button(self.root, text='Segmented', command= lambda: edit.segmented(self.image))
         segmented_button.pack(expand=True)
 
     def show_image(self):
-        self.filtered_image.thumbnail((500, 500))
-        photo = ImageTk.PhotoImage(self.filtered_image)
+        self.preview.thumbnail((500, 500))
+        photo = ImageTk.PhotoImage(self.preview)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
         self.canvas.image = photo
 
@@ -76,8 +77,8 @@ class Window:
             filetypes=filetypes
         )
         if self.filename:
-            self.original_image = Image.open(self.filename)
-            self.filtered_image = self.original_image.copy()
+            self.image = Image.open(self.filename)
+            self.preview = self.image.copy()
             showinfo(title='Selected File', message=self.filename)
             self.show_image()
         else:
